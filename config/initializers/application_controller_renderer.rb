9db76150ef2077714591387b8e0e8c88a4b2c919
Takada-Aoi <a.m.3m0221@gmail.com>
2021-04-29 16:18:53 +0000

    [Add]2

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '0c3f013148dace9960da4020861c3f16e4d2e9fdf8c0f9ada423074a727d2323714678252a034145f9b3ab24d7e996d02ba3af6cac2ba2a8f9f24bcc94df3955'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
+  resources :users, only:[:show,:index,:edit,:update,:destroy]
   
 end
