076b269e53cd22e2836746d80e6f65211687b7eb
Takada-Aoi <a.m.3m0221@gmail.com>
2021-05-11 15:00:27 +0000

    [Add]4

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
-  resources :users, only:[:show,:index,:edit,:update,:destroy]
+  resources :users, only:[:show,:index,:edit,:update,:destroy] do
+    resource :relationships, only:[:create,:destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end  
   resources :books, except:[:new]
   
 end
